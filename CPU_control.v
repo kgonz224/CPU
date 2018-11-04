1c16a9332cf82f6dab219c57bf5a686a7dd41484
dianaliz93 <djard005@fiu.edu>
2018-11-04 14:54:39 -0500

    Update CPU_control.v

--- CPU_control.v
+++ CPU_control.v
@@ -4,7 +4,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 	input[10:0] inst31_21;
 	output[1:0] ALUOp;
 	output Reg2Loc, Branch, MemRead, MemtoReg;
-        output MemWrite, ALUSrc, RegWrite;
+    output MemWrite, ALUSrc, RegWrite;
 	
 	wire [1:0] ALUOp;
 	wire Reg2Loc, Branch, MemRead, MemtoReg;
@@ -12,7 +12,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 	
 	
 	always @ (*) begin 
-		if (inst31_21 == 11'b11111000010) begin //LDUR 
+		if 		(inst31_21 == 11'b11111000010) begin //LDUR 
 				Reg2Loc <= x; // ??
 				Branch <= 0;
 				MemRead <= 1;
@@ -22,7 +22,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 				ALUSrc <= 1;
 				RegWrite <= 1;
 		end
-		else if inst31_21 == 11'b11111000000) begin //STUR 
+		else if (inst31_21 == 11'b11111000000) begin //STUR 
 				Reg2Loc <= 1;
 				Branch <= 0;
 				MemRead <= 0;
@@ -32,7 +32,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 				ALUSrc <= 1;
 				RegWrite <= 0;
 		end
-		else if inst31_21 == 11'b10001011000) begin //ADD 
+		else if (inst31_21 == 11'b10001011000) begin //ADD 
 				Reg2Loc <= 0;
 				Branch <= 0;
 				MemRead <= 0;
@@ -42,7 +42,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 				ALUSrc <= 0;
 				RegWrite <= 1;
 		end
-		else if inst31_21 == 11'b11001011000) begin //SUB
+		else if (inst31_21 == 11'b11001011000) begin //SUB
 				Reg2Loc <= 0;
 				Branch <= 0;
 				MemRead <= 0;
@@ -52,7 +52,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 				ALUSrc <= 0;
 				RegWrite <= 1;
 		end
-		else if inst31_21 == 11'b10001010000) begin //AND
+		else if (inst31_21 == 11'b10001010000) begin //AND
 				Reg2Loc <= 0;
 				Branch <= 0;
 				MemRead <= 0;
@@ -62,7 +62,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 				ALUSrc <= 0;
 				RegWrite <= 1;
 		end
-		else if inst31_21 == 11'b10101010000 ) begin //ORR
+		else if (inst31_21 == 11'b10101010000 ) begin //ORR
 				Reg2Loc <= 0;
 				Branch <= 0;
 				MemRead <= 0;
@@ -72,15 +72,45 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 				ALUSrc <= 0;
 				RegWrite <= 1;
 		end 
-		else if inst31_21 == 11'b11111111111 ) begin //HALT
+		else if (inst31_21 == 11'b10110100XXX ) begin //CBZ
+				Reg2Loc <= 1;
+				Branch <= 0;
+				MemRead <= 0;
+				MemtoReg <= 0; 
+				//ALUOp <= 2'b10;
+				MemWrite <= 0;
+				ALUSrc <= 0;
+				RegWrite <= 1;
+		end 
+		else if (inst31_21 == 11'b10110101XXX ) begin //CBNZ
+				Reg2Loc <= 1;
+				Branch <= 1;
+				MemRead <= 0;
+				MemtoReg <= 0; 
+				//ALUOp <= 2'b10;
+				MemWrite <= 0;
+				ALUSrc <= 0;
+				RegWrite <= 1;
+		end 
+		else if (inst31_21 == 11'b000101XXXXX ) begin //B
 				Reg2Loc <= 0;
 				Branch <= 0;
 				MemRead <= 0;
 				MemtoReg <= 0; 
-				ALUOp <= 2'b10;
+				//ALUOp <= 2'b10;
 				MemWrite <= 0;
 				ALUSrc <= 0;
-				RegWrite <= 1;
+				RegWrite <= 0;
+		end 
+		else if (inst31_21 == 11'b11111111111 ) begin //HALT
+				Reg2Loc <= 0;
+				Branch <= 0;
+				MemRead <= 0;
+				MemtoReg <= 0; 
+				//ALUOp <= 2'b10;
+				MemWrite <= 0;
+				ALUSrc <= 0;
+				RegWrite <= 0;
 		end 
 		else begin
                 Reg2Loc <= 0;
@@ -90,8 +120,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, MemRead, MemtoReg
 				MemWrite <= 0;
 				ALUSrc <= 0;
 				RegWrite <= 0;  
-      end
-	
+        end	
 endmodule
 
 
