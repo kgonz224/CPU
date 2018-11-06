b9ba080b2967d6148afef847c15f623a26fdfdc4
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-06 00:08:38 -0500

    Update Execution.v

--- Execution.v
+++ Execution.v
@@ -6,13 +6,13 @@ module Execution(Address, Instruction, signExtInstr, Data1, Data2, ALUSrc,
 	Reg2Write, oldRegWrite, oldBranchAddress, PCSrc);
 
   input /*reg*/ [1:0] ALUSrc, ALUOp;
-  input /*reg*/ B, BZ, BNZ, MemWrite, MemtoReg, RegWrite;
+  input /*reg*/ B, BZ, BNZ, MemWrite, MemRead, MemtoReg, RegWrite;
   input /*reg*/ [31:0] Instruction;
   input /*reg*/ [63:0] Address, signExtInstr;
   output wire oldRegWrite, PCSrc;
   output wire [4:0] Reg2Write;
   output wire [63:0] oldBranchAddress, Data2Write;
-  wire [63:0] ALUInput2, branchAddress, Results;
+	wire [63:0] ALUInput2, branchAddress, Results, Data1, Data2;
   wire [3:0] ALUInstr;
   wire zero;
 
