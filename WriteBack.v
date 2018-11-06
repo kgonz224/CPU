c8ec4cf6188d58d93f006d670d403f2883ac0dd4
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-05 23:07:27 -0500

    Update WriteBack.v

--- WriteBack.v
+++ WriteBack.v
@@ -1,9 +1,9 @@
 module WriteBack(Reg, loadedData, Results, MemToReg, RegWrite,
 	  Data2Write, Reg2Write, oldRegWrite);
 
-  input reg [4:0] Reg;
-  input reg [63:0] loadedData, Results;
-  input reg MemToReg, RegWrite;
+  input /*reg*/ [4:0] Reg;
+  input /*reg*/ [63:0] loadedData, Results;
+  input /*reg*/ MemToReg, RegWrite;
   output wire [63:0] Data2Write;
   output wire [4:0] Reg2Write;
   output wire oldRegWrite;
