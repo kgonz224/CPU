5430e68cd3e8a9795715f2f93ac36e742507fd1a
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-06 13:46:07 -0500

    Update ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -4,6 +4,7 @@ module alu_control (inst31_21, ALUOp, control_line);
 	output reg[3:0]control_line;
 	
 	always begin 
+		#1
 		if ( ALUOp == 00) begin // LDUR & STUR
 			control_line <= 4'b0010;
 		end
