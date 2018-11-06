611a786ccb7b9473cf612978c3161c630082b0af
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-05 23:10:57 -0500

    Update MemoryAccess.v

--- MemoryAccess.v
+++ MemoryAccess.v
@@ -4,9 +4,9 @@ module MemoryAccess(Instruction, branchAddress, Results, Data2, zero, B, BZ,
   	BNZ, MemRead, MemWrite, MemToReg, RegWrite, oldBranchAddress, PCSrc,
 	oldRegWrite, Data2Write, Reg2Write);
 
-  input reg zero, B, BZ, BNZ, MemRead, MemWrite, MemToReg, RegWrite;
-  input reg [63:0] branchAddress, Results, Data2;
-  input reg [31:0] Instruction;
+  input /*reg*/ zero, B, BZ, BNZ, MemRead, MemWrite, MemToReg, RegWrite;
+  input /*reg*/ [63:0] branchAddress, Results, Data2;
+  input /*reg*/ [31:0] Instruction;
   output PCSrc, oldRegWright;
   output [4:0] Reg2Write;
   output [63:0] oldBranchAddress, Data2Write;
