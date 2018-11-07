b5b4632f6201c4f102a20b106359b1d40db9443b
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-06 19:34:23 -0500

    Update InstructionDecode.v

--- InstructionDecode.v
+++ InstructionDecode.v
@@ -35,9 +35,21 @@ module InstructionDecode(Instruction, Address, PCSrc, BranchAddress);
 	  else
                   Data2[7:0] = Regs[Instruction[4:0]];
 
-
-	  signExtInstr[31:0] = Instruction;
-	  signExtInstr[63:32] = {32{Instruction[31]}};
+	  if(B)
+	  begin
+		  signExtInstr[25:0] = Instruction[25:0];
+		  signExtInstr[63:45] = {38{Instruction[25]}};  
+	  end
+	  else if(BZ | BNZ)
+	  begin		  
+		  signExtInstr[18:0] = Instruction[23:5];
+		  signExtInstr[63:45] = {45{Instruction[23]}};
+	  end
+	  else
+	  begin
+		  signExtInstr[9:0] = Instruction[20:11];
+		  signExtInstr[63:10] = {54{Instruction[20]}};
+	  end
 	  $display("\tsignExtInstr: %64b ", signExtInstr);
 
   end
