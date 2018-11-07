247256292d5853b3e2920dfad90430c07c990e59
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-06 19:53:59 -0500

    Update InstructionFetch.v

--- InstructionFetch.v
+++ InstructionFetch.v
@@ -11,7 +11,6 @@ module InstructionFetch; // processor test bench template
 //  reg [7:0] DMem[8191:0]; // 8192 bytes (1024 double words)
   reg [31:0] instruction; // all instructions are 32-bit wide
   reg [63:0] PC; // PC contains 64-bit byte address
-  integer i;
 	
   initial // load instruction memory and data memory
   begin 
@@ -20,7 +19,6 @@ module InstructionFetch; // processor test bench template
 	$readmemh("IM_Bytes.txt", IMem);
 //	$readmemh("DM_Bytes.txt", DMem);
 	PC = 64'b0; // initialize PC
-	i = 0;
   end 
  
   InstructionDecode id(instruction, PC, PCSrc, BranchAddress);
@@ -28,15 +26,14 @@ module InstructionFetch; // processor test bench template
   always //sequential logic of fetch for illustration
   begin
 	#1
-	  i = i + 1;
 	// this code block can be performed in any other module
 	// concatenate four bytes of IMem into PC
 	  instruction[7:0] = IMem[PC];
 	  instruction[15:8] = IMem[PC + 1];
 	  instruction[23:16] = IMem[PC + 2];
 	  instruction[31:24] = IMem[PC + 3];
-	  $display("Opcode value: %32b %4d \nAddress: %64b \n", instruction[31:0], i, PC);
-	  #100
+	  $display("Opcode value: %32b \n", instruction[31:0]);
+	  #10
 	if (PCSrc == 0)
 	begin
 		PC = PC + 4; // PC needs to be updated in the processor/datapath module
@@ -50,7 +47,7 @@ module InstructionFetch; // processor test bench template
   // output data memory to a file when HALT instruction is fetched
   always @(instruction) 
   begin
-	  if (instruction[31:21] == {11{1'b1}} | i == 30)
+	  if (instruction[31:21] == {11{1'b1}})
     begin
 	$display("final opcode is detected \n");
 //	$writememh("DM_Final_Bytes.txt", DMem);
