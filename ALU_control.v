c3899b66677e8fdc4f8fd014098d6d82461cd840
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-09 21:01:43 -0500

    Update ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -4,26 +4,27 @@ module alu_control (inst31_21, ALUOp, control_line);
 	output reg[3:0]control_line;
 	
 	always @(*) begin 
-		#11
-		if ( ALUOp == 2'b00) begin // LDUR & STUR
-			control_line[3:0] <= 4'b0010;
-		end
-		else if ( ALUOp == 01) begin
-			control_line[3:0] <= 4'b0111;
-		end 
-		else if (inst31_21 == 11001011000 && ALUOp == 10)begin //sub
-			control_line[3:0] <= 4'b0110;
-		end 
-		else if (inst31_21 == 10001010000 && ALUOp == 10)begin //and
-			control_line[3:0] <= 4'b0000;
-		end 
-		else if (inst31_21 == 10101010000 && ALUOp == 10)begin //orr
-			control_line[3:0] <= 4'b0001;
-		end 
-		else if (ALUOp == 10)begin //add, addi, branch, etc
-			control_line[3:0] <= 4'b0010;
-		end
-    end
+		#1
+		case(ALUOp)
+			2'b00: // LDUR & STUR
+				control_line[3:0] <= 4'b0010;
+			2'b01: //AND
+				control_line[3:0] <= 4'b0111;
+			2'b10:
+			begin
+				case(inst31_21)
+					11'b11001011000: //sub
+						control_line[3:0] <= 4'b0110;
+					11'b10001010000: //and
+						control_line[3:0] <= 4'b0000;
+					11'b10101010000: //orr
+						control_line[3:0] <= 4'b0001;
+					default: //add, addi, branch, etc
+						control_line[3:0] <= 4'b0010;
+				endcase
+			end
+		endcase
+    	end
 	
 endmodule 
 			
