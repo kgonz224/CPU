1c67962b041a064b7dfd9a14c030260bde39e255
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-17 13:43:29 -0500

    Update InstructionFetch.v

--- InstructionFetch.v
+++ InstructionFetch.v
@@ -20,10 +20,10 @@ module InstructionFetch; // processor test bench template
 //	$readmemh("DM_Bytes.txt", DMem);
 	PC = 64'b0; // initialize PC
 //	First Instruction is NOP until instructions are loaded into PC
-	instruction[7:0] <= 8'b11010101;
-	instruction[15:8] <= 8'b00000011;
-	instruction[23:16] <= 8'b00100000;
-	instruction[31:24] <= 8'b00011111;
+	  instruction[7:0] = 8'b11010101;
+	instruction[15:8] = 8'b00000011;
+	instruction[23:16] = 8'b00100000;
+	  instruction[31:24] = 8'b00011111;
   end 
  
   InstructionDecode id(instruction, PC, PCSrc, BranchAddress);
