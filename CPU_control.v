b66a44eff1dc14318a6ae64d817540818fdb1333
Kevin Gonzalez <35079409+kgonz224@users.noreply.github.com>
2018-11-18 14:17:49 -0500

    Update CPU_control.v

--- CPU_control.v
+++ CPU_control.v
@@ -7,6 +7,7 @@ module cpu_control (inst31_21,Reg2Loc, Branch, BranchZero, BranchNonZero,
     	output reg MemWrite, RegWrite;
 	
 	always @ (inst31_21) begin 
+		$display("\tOh\n");
 		case(inst31_21)
 			11'b11111000010: //LDUR
 			begin
